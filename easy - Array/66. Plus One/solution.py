27491134ae80826776deffe5541b80b23f583726
Wenjia Zhai <vincent507cpu@gmail.com>
2020-05-11 07:55:53 -0400

    update to may 11

---
+++ easy - Array/66. Plus One/solution.py
@@ -0,0 +1,46 @@
+class Solution:
+    # my initial solution
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = ''.join([str(i) for i in digits])
+        num = int(num) + 1
+        num = str(num)
+        return [int(s) for s in num]
+    
+    # a slightly better solution
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] += 1
+        
+        for i in range(len(digits)-1, 0, -1):
+            if digits[i] != 10:
+                break
+            digits[i] = 0
+            digits[i-1] += 1
+            
+        if digits[0] == 10:
+            digits[0] = 0
+            digits = [1] + digits
+            
+        return digits
+    
+    # recursion
+    # https://leetcode.com/problems/plus-one/discuss/24090/Python-simple-solution-using-recursion
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if len(digits) == 0:
+            return [1]
+        elif digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+        else:
+            digits = self.plusOne(digits[:-1])
+            digits.append(0)
+        return digits
+    
+    # better recursion from same page
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = digits or [0]
+        num = digits.pop()
+        
+        if num == 9:
+            return self.plusOne(digits) + [0]
+        else:
+            return digits + [num+1]
